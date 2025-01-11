e7a6ad9e6782c94a058d1b7af98d0df813be5e75
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-01-11 23:49:24 +0300

    ModelAndView

--- src/main/java/com/mehmetsukrukavak/springbootmvc/HomeController.java
+++ src/main/java/com/mehmetsukrukavak/springbootmvc/HomeController.java
@@ -6,12 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "index.jsp";
+        return "index";
     }
 
 //    @RequestMapping("add")
@@ -32,10 +33,18 @@ public class HomeController {
 //        return "result.jsp";
 //    }
 
+//    @RequestMapping("add")
+//    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
+//        int result = num + num2;
+//        model.addAttribute("result", result) ;
+//        return "result";
+//    }
+
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num, @RequestParam("num2") int num2, ModelAndView mv){
         int result = num + num2;
-        model.addAttribute("result", result) ;
-        return "result.jsp";
+        mv.addObject("result", result) ;
+        mv.setViewName("result");
+        return mv;
     }
 }
