076deb5680938979193d397da268e8b654ae886e
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-01-11 16:41:08 +0300

    RequestParam

--- src/main/java/com/mehmetsukrukavak/springbootmvc/HomeController.java
+++ src/main/java/com/mehmetsukrukavak/springbootmvc/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -12,14 +13,21 @@ public class HomeController {
         return "index.jsp";
     }
 
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest request, HttpSession session){
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+//        int result = num1 + num2;
+//
+//        session.setAttribute("result", result);
+//
+//        return "result.jsp";
+//    }
+
     @RequestMapping("add")
-    public String add(HttpServletRequest request, HttpSession session){
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
-        int result = num1 + num2;
-
+    public String add(@RequestParam("num1") int num, @RequestParam("num2") int num2, HttpSession session){
+        int result = num + num2;
         session.setAttribute("result", result);
-
         return "result.jsp";
     }
 }
